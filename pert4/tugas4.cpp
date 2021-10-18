bf95f434c24931c9a2501ad9ec93fa3ea570a4b9
root <root@DESKTOP-N99R6IE.localdomain>
2021-10-18 21:01:59 +0700

    first commit

---
+++ pert4/tugas4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class umur{
+    private:
+    int a, b;
+
+    public:
+    void assign(int tahunlahir, int tahunsekarang){
+        a=tahunlahir;
+        b=tahunsekarang;
+    }
+    int usia(){
+        return a-b;
+    }
+};
+
+int main(){
+    umur obj;
+    obj.assign(2021, 1990);
+    cout<<"Berarti Using Kalian sekarang adalah "<<obj.usia()<<" Tahun"<<endl;
+    return 0;
+}
